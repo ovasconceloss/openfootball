56b001dca7e6a11150a2fc6776e3b0795364ae5c
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-28 22:10:49 +0100

    feat: create implementation of from_str for position struct

--- src-tauri/src/core/domain/enums/position.rs
+++ src-tauri/src/core/domain/enums/position.rs
@@ -1,4 +1,8 @@
+use std::str::FromStr;
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug)]
+pub struct ParsePositionError;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Position {
@@ -17,4 +21,27 @@ pub enum Position {
   LeftWinger,
   Striker,
   SecondStriker
+}
+
+impl FromStr for Position {
+  type Err = ParsePositionError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s.to_lowercase().as_str() {
+      "goalkeeper" => Ok(Position::Goalkeeper),
+      "rigthback" => Ok(Position::RightBack),
+      "leftback" => Ok(Position::LeftBack),
+      "centerback" => Ok(Position::CenterBack),
+      "defensivemidfielder" => Ok(Position::DefensiveMidfielder),
+      "centralmidfielder" => Ok(Position::CentralMidfielder),
+      "attackingmidfielder" => Ok(Position::AttackingMidfielder),
+      "rightmidfielder" => Ok(Position::RightMidfielder),
+      "leftmidfielder" => Ok(Position::LeftMidfielder),
+      "rightwinger" => Ok(Position::RightWinger),
+      "leftwinger" => Ok(Position::LeftWinger),
+      "striker" => Ok(Position::Striker),
+      "secondstriker" => Ok(Position::SecondStriker),
+      _ => Err(ParsePositionError),
+    }
+  }
 }
